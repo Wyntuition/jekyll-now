0bab73f28729db9bea0bf486a3f2df8bbeec43ac
Wyn Van Devanter <wyn.vandevanter@excella.com>
2018-05-08 17:35:45 -0400

    todo

--- aliases.sh
+++ aliases.sh
@@ -2,11 +2,11 @@
 # Can just put a ref to this in ~/.bashrc etc: source ~/dev/wyntuition.github.io/aliases.sh
 shopt -s expand_aliases
 
-alias save-ui="code ~/dev/uscis/save-ui"
-alias save-cv="code ~/dev/uscis/save-case-verification"
-alias wyn="code ~/dev/wyntuition.github.io"
-alias ms="code ~/dev/microservices-sandbox/"
-alias scala="code ~/dev/scala"
-alias etl="code ~/dev/uscis/vis-case-processing-etl"
+alias save-ui="cd ~/dev/uscis/save-ui && code ."
+alias save-cv="cd ~/dev/uscis/save-case-verification && code ."
+alias wyn="cd ~/dev/wyntuition.github.io && code ."
+alias ms="cd ~/dev/microservices-sandbox/ && code ."
+alias scala="cd ~/dev/scala && code ."
+alias etl="cd ~/dev/uscis/vis-case-processing-etl && code ."
 
-alias todo="code ~/dev/wyntuition.github.io/todo/01.md"
+alias todo="cd ~/dev/wyntuition.github.io/todo/01.md && code ."
